749948084ab734b5a3cdddd3eb5ef65fe8085430
Ole Gulbrandsen <o.gulbrandsen@gmail.com>
2022-06-15 07:18:08 +0200

    Refinements

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -7,19 +7,19 @@ using WAF.Engine.Content.Native;
 
 namespace App.Server.Controllers {
     public class AccountController : Controller {
-        WAFControllerContext _ctx;
+        readonly WAFControllerContext _ctx;
         public AccountController(WAFControllerContext ctx) {
             _ctx = ctx;
         }
         public async Task<IActionResult> Login(LoginModel? login) {
             if (login == null || login.UserName == null) return View();
             var clientAddress = HttpContext.Connection.RemoteIpAddress?.ToString();
-
             if (_ctx.Engine.TryLogInSession(_ctx.Session, login.UserName, login.Password, clientAddress, 
-                null, null, out var failureReson, out var meta, out var code, out var deviceKey)) {
+                null, null, out var failureReson, out _, out _, out _)) {
 
-                var claims = new List<Claim>();
-                claims.Add(new Claim(ClaimTypes.Name, _ctx.Session.UserName));
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, _ctx.Session.UserName)
+                };
                 var identity = new ClaimsIdentity(claims, _ctx.Config.AuthenticationSchemaName);
                 var claimsPrinciple = new ClaimsPrincipal(identity);
                 await HttpContext.SignInAsync(_ctx.Config.AuthenticationSchemaName, claimsPrinciple);
